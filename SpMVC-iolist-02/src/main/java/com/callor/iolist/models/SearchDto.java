288ab0cf8db15e24fc2efcbc2a1e3a5e5595a3c6
unknown <ckw2434@naver.com>
2024-04-05 11:25:36 +0900

    log

---
+++ SpMVC-iolist-02/src/main/java/com/callor/iolist/models/SearchDto.java
@@ -0,0 +1,21 @@
+package com.callor.iolist.models;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchDto {
+	
+	private String pname;
+	private String sdate;
+	private String edate;
+	
+
+}
