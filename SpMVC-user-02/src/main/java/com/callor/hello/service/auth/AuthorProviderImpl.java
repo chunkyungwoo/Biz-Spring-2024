c6ced5a3190496635971a620642f8e9f1ff1dcfb
unknown <ckw2434@naver.com>
2024-04-12 15:35:24 +0900

    login

--- SpMVC-user-02/src/main/java/com/callor/hello/service/auth/AuthorProviderImpl.java
+++ SpMVC-user-02/src/main/java/com/callor/hello/service/auth/AuthorProviderImpl.java
@@ -6,11 +6,13 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service("authorProviderImpl")
@@ -21,12 +23,22 @@ public class AuthorProviderImpl implements AuthenticationProvider{
 		String u_name = authentication.getName();
 		String u_password = authentication.getCredentials().toString();
 		
+		// 로그인한 사용자 id(u_name) 을 검사하여
+		// 허가받은 사용자인가 확인하여 아니면 exception 을 발생하여
+		// 알려주기
+		if(u_name == null || u_name.isBlank() || !u_name.equals("callor")) {
+			throw new UsernameNotFoundException("USERNAEM 없음");
+		}
+		if(u_password == null || u_password.isBlank() || !u_password.equalsIgnoreCase("12345")) {
+			throw new BadCredentialsException("비밀번호 오류");
+		}
+		
 		Map<String, String> user = new HashMap<>();
 		user.put("username", u_name);
 		user.put("email", "callor@callor.com");
 		
 		List<GrantedAuthority> grantList = new ArrayList<>();
-		grantList.add(new SimpleGrantedAuthority("ADMIN"));
+//		grantList.add(new SimpleGrantedAuthority("ADMIN"));
 		grantList.add(new SimpleGrantedAuthority("USER"));
 		
 		UsernamePasswordAuthenticationToken token = 
