0c12bffa71c7d10dbec21fc04c0eca48650fee91
unknown <ckw2434@naver.com>
2024-05-30 13:48:45 +0900

    food

---
+++ Food/src/main/java/com/callor/food/models/RoleVO.java
@@ -0,0 +1,21 @@
+package com.callor.food.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoleVO {
+	
+	private String r_mid;
+	private String r_role;
+
+}
---
+++ Food/src/main/java/com/callor/food/service/impl/UserServiceImpl.java
@@ -0,0 +1,79 @@
+package com.callor.food.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.callor.food.models.FoodVO;
+import com.callor.food.models.RoleVO;
+import com.callor.food.models.UserVO;
+import com.callor.food.persistance.RoleDao;
+import com.callor.food.persistance.UserDao;
+import com.callor.food.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+	
+	
+	private final PasswordEncoder passwordEncoder;
+	private final UserDao userDao;
+	private final RoleDao roleDao;
+	
+	public UserServiceImpl(@Qualifier("passEncoder") PasswordEncoder passwordEncoder, UserDao userDao, RoleDao roleDao) {
+		super();
+		this.passwordEncoder = passwordEncoder;
+		this.userDao = userDao;
+		this.roleDao = roleDao;
+		
+	}
+	
+
+	@Override
+	public UserVO createUser(UserVO createUserVO) {
+		String m_id = createUserVO.getM_id();
+		String m_password = createUserVO.getM_password();
+		
+		List<UserVO> userList = userDao.selectAll();
+		
+		String encPassword = passwordEncoder.encode(m_password);
+		createUserVO.setM_password(encPassword);
+		
+		List<RoleVO> roles = new ArrayList<>();
+		if(userList == null || userList.size() <= 0) {
+			roles.add(RoleVO.builder().r_mid(m_id).r_role("ROLE_ADMIN").build());
+			roles.add(RoleVO.builder().r_mid(m_id).r_role("ROLE_USER").build());
+		}else {
+			roles.add(RoleVO.builder().r_mid(m_id).r_role("ROLE_USER").build());
+		}
+		userDao.insert(createUserVO);
+		roleDao.insertAll(roles);
+		return null;
+	}
+
+	@Autowired
+	public void create_table() {
+		
+		userDao.create_user_table(null);
+		userDao.create_role_table(null);
+		
+	}
+	
+	@Override
+	public UserVO findById(String m_id) {
+		
+		return userDao.findById(m_id);
+	}
+
+	@Override
+	public FoodVO selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
