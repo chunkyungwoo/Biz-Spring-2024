fc983413978c4482884c054f2ccdb4adee56d99f
unknown <ckw2434@naver.com>
2024-05-31 15:32:44 +0900

    gallery

---
+++ SpMVC-gallery-05/document/database(2024-05-31).sql
@@ -0,0 +1,9 @@
+-- galleryDB3
+DROP DATABASE galleryDB3;
+CREATE DATABASE galleryDB3;
+USE galleryDB3;
+SELECT * FROM tbl_gallerys;
+SELECT * FROM tbl_images;
+
+DROP TABLE tbl_images;
+DROP TABLE tbl_gallerys;
